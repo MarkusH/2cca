8ecc63546039e0ed6e55591a91dc458fc8a142bd
Markus Holtermann <info@markusholtermann.eu>
2018-03-27 23:21:52 +0200

    Make Python3 aware and general flake8 fixes

--- 2cca.py
+++ 2cca.py
@@ -1,19 +1,20 @@
+#!/usr/bin/env python
 # -*- coding: utf-8 -*-
 import os
 import sys
 
-defaults={
+defaults = {
     'root': {
-        'days':3650,
-        'ou':'RootCA',
+        'days': 3650,
+        'ou': 'RootCA',
         'extensions': {
             'basicConstraints': 'critical,CA:true,pathlen:1',
             'keyUsage': 'critical,keyCertSign,cRLSign'
         }
     },
     'sub':  {
-        'days':3649,
-        'ou':'CA',
+        'days': 3649,
+        'ou': 'CA',
         'extensions': {
             'basicConstraints': 'critical,CA:true,pathlen:1',
             'keyUsage': 'critical,keyCertSign,cRLSign'
@@ -21,11 +22,11 @@ defaults={
     },
     'server': {
         'days': 3648,
-        'ou':'server',
+        'ou': 'server',
         'extensions': {
             'basicConstraints': 'critical,CA:false',
             'keyUsage': 'digitalSignature, keyEncipherment',
-            'extendedKeyUsage':'serverAuth'
+            'extendedKeyUsage': 'serverAuth'
         }
     },
     'client': {
@@ -34,7 +35,7 @@ defaults={
         'extensions': {
             'basicConstraints': 'critical,CA:false',
             'keyUsage': 'digitalSignature',
-            'extendedKeyUsage':'clientAuth'
+            'extendedKeyUsage': 'clientAuth'
         }
     },
     'www': {
@@ -48,60 +49,63 @@ defaults={
     }
 }
 
+
 def run(cmd):
-    print cmd
+    print(cmd)
     os.system(cmd)
+
 
 def openssl_ecc_supported():
     supp = []
     p = os.popen('openssl ecparam -list_curves', 'r')
     for line in p.readlines():
         fields = line.split(':')
-        if len(fields)==2:
+        if len(fields) == 2:
             supp.append(fields[0].strip())
     p.close()
     return supp
 
+
 def get_config(args):
-    ec_supported=None
-    cmd=args[0]
+    ec_supported = None
+    cmd = args[0]
     cfg = {'command': cmd}
     for arg in args[1:]:
         fields = arg.split('=')
-        if len(fields)==1:
-            cfg[arg]=True
+        if len(fields) == 1:
+            cfg[arg] = True
             continue
-        if len(fields)!=2:
+        if len(fields) != 2:
             continue
-        if fields[0]=='alt':
-            if cfg.get('alt')==None:
-                cfg['alt']=list()
+        if fields[0] == 'alt':
+            if cfg.get('alt') is None:
+                cfg['alt'] = list()
             cfg['alt'].append(fields[1])
-        elif fields[0]=='ecc':
+        elif fields[0] == 'ecc':
             if not ec_supported:
                 ec_supported = openssl_ecc_supported()
             if not fields[1] in ec_supported:
-                print 'unsupported curve:', fields[1]
-                print 'supported curves:'
-                print ec_supported
+                print('unsupported curve:', fields[1])
+                print('supported curves:')
+                print(ec_supported)
                 raise SystemExit
-            cfg['ecc']=fields[1]
+            cfg['ecc'] = fields[1]
         else:
-            cfg[fields[0].lower()]=fields[1]
+            cfg[fields[0].lower()] = fields[1]
     # Consistency checks
     if cmd in defaults.keys():
-        if cfg.get('cn')==None:
-            print 'Specify a common name with cn=NAME'
+        if cfg.get('cn') is None:
+            print('Specify a common name with cn=NAME')
             raise SystemExit
-        if cfg.get('days')==None:
-            cfg['days']=defaults[cmd]['days']
-        if cfg.get('ou')==None:
-            cfg['ou']=defaults[cmd]['ou']
+        if cfg.get('days') is None:
+            cfg['days'] = defaults[cmd]['days']
+        if cfg.get('ou') is None:
+            cfg['ou'] = defaults[cmd]['ou']
     if cmd in ['sub', 'server', 'client', 'www', 'crl', 'revoke']:
-        if cfg.get('ca')==None:
-            print 'Specify a CA to use for this operation with ca=NAME'
+        if cfg.get('ca') is None:
+            print('Specify a CA to use for this operation with ca=NAME')
             raise SystemExit
-    cfg['ext']='''
+    cfg['ext'] = '''
         [req]
         distinguished_name=subject
         x509_extensions=v3
@@ -111,9 +115,9 @@ def get_config(args):
 ''' % cfg
     for elem in ['c', 'o', 'ou', 'st', 'l']:
         if cfg.get(elem):
-            cfg['ext']+=elem.upper()+'='+cfg[elem]+'\n'
+            cfg['ext'] += elem.upper()+'='+cfg[elem]+'\n'
 
-    cfg['ext']+='''
+    cfg['ext'] += '''
         [v3]
         subjectKeyIdentifier = hash
         authorityKeyIdentifier = keyid,issuer
@@ -122,73 +126,79 @@ def get_config(args):
     if cfg['command'] in ['root', 'sub', 'server', 'client', 'www']:
         extensions = defaults[cfg['command']]['extensions']
         for ext in extensions.keys():
-            cfg['ext']+='%s=%s\n' % (ext, extensions[ext])
+            cfg['ext'] += '%s=%s\n' % (ext, extensions[ext])
 
     # Factorize alt into SAN
     if cfg.get('alt'):
-        cfg['ext']+='''
+        cfg['ext'] += '''
 subjectAltName=@alt_names
 [alt_names]
 '''
         for i in range(len(cfg['alt'])):
-            cfg['ext']+='DNS.%d = %s\n' % (i+1, cfg['alt'][i])
+            cfg['ext'] += 'DNS.%d = %s\n' % (i+1, cfg['alt'][i])
 
-    f=open(cfg['cn']+'.cnf', 'w')
+    f = open(cfg['cn']+'.cnf', 'w')
     for line in (cfg['ext']).split('\n'):
         f.write(line.strip()+'\n')
     f.close()
     return cfg
 
+
 def generate_serial():
-    f=open('/dev/urandom', 'rb')
-    data=f.read(14)
+    f = open('/dev/urandom', 'rb')
+    data = f.read(14)
     f.close()
-    num='0x2cca'
+    num = '0x2cca'
     for r in data:
-        num+='%02x' % ord(r)
+        num += '%02x' % ord(r)
     return num
+
 
 def genkey(cfg):
     # Generate key pair
-    keycmd=''
+    keycmd = ''
     if cfg.get('ecc'):
-        keycmd='openssl ecparam -genkey -name %(ecc)s -out "%(cn)s.key"' % cfg
+        keycmd = 'openssl ecparam -genkey -name %(ecc)s -out "%(cn)s.key"' % cfg
     elif cfg.get('rsa'):
-        keycmd='openssl genrsa -out "%(cn)s.key" %(rsa)s' % cfg
+        keycmd = 'openssl genrsa -out "%(cn)s.key" %(rsa)s' % cfg
     else:
-        keycmd='openssl genrsa -out "%(cn)s.key" 2048' % cfg
+        keycmd = 'openssl genrsa -out "%(cn)s.key" 2048' % cfg
     run(keycmd)
 
+
 def gencsr(cfg):
-    cmd='openssl req -new -sha256 -key "%(cn)s.key" -out "%(cn)s.csr"' % cfg
-    cmd+=' -config "%(cn)s.cnf"' % cfg
-    cmd+=' -extensions v3'
+    cmd = 'openssl req -new -sha256 -key "%(cn)s.key" -out "%(cn)s.csr"' % cfg
+    cmd += ' -config "%(cn)s.cnf"' % cfg
+    cmd += ' -extensions v3'
     run(cmd)
 
+
 def gencrt(cfg):
-    cmd='openssl x509 -req -sha256'
-    cmd+=' -CA "%(ca)s.crt" -CAkey "%(ca)s.key"' % cfg
-    cmd+=' -in "%(cn)s.csr" -out "%(cn)s.crt"' % cfg
-    cmd+=' -set_serial %s' % generate_serial()
+    cmd = 'openssl x509 -req -sha256'
+    cmd += ' -CA "%(ca)s.crt" -CAkey "%(ca)s.key"' % cfg
+    cmd += ' -in "%(cn)s.csr" -out "%(cn)s.crt"' % cfg
+    cmd += ' -set_serial %s' % generate_serial()
     if cfg.get('days'):
-        cmd+=' -days %(days)s' % cfg
-    cmd+=' -extfile "%(cn)s.cnf"' % cfg
-    cmd+=' -extensions v3'
+        cmd += ' -days %(days)s' % cfg
+    cmd += ' -extfile "%(cn)s.cnf"' % cfg
+    cmd += ' -extensions v3'
     run(cmd)
+
 
 def generate_root(cfg):
     # Generate key pair
     genkey(cfg)
     # Generate self-signed certificate
-    cmd='openssl req -new -x509 -key "%(cn)s.key"' % cfg
-    cmd+=' -extensions v3'
-    cmd+=' -config "%(cn)s.cnf"' % cfg
-    cmd+=' -sha256'
-    cmd+=' -out "%(cn)s.crt"' % cfg
-    cmd+=' -set_serial %s' % generate_serial()
-    cmd+=' -days %(days)s' % cfg
+    cmd = 'openssl req -new -x509 -key "%(cn)s.key"' % cfg
+    cmd += ' -extensions v3'
+    cmd += ' -config "%(cn)s.cnf"' % cfg
+    cmd += ' -sha256'
+    cmd += ' -out "%(cn)s.crt"' % cfg
+    cmd += ' -set_serial %s' % generate_serial()
+    cmd += ' -days %(days)s' % cfg
     run(cmd)
-    #os.remove('%(cn)s.cnf' % cfg)
+    # os.remove('%(cn)s.cnf' % cfg)
+
 
 def generate_identity(cfg):
     # Generate key pair
@@ -201,15 +211,18 @@ def generate_identity(cfg):
     os.remove('%(cn)s.cnf' % cfg)
     os.remove('%(cn)s.csr' % cfg)
 
+
 def crl_show(cfg):
-    print cfg
+    print(cfg)
+
 
 def revoke(cfg):
-    print cfg
+    print(cfg)
 
-if __name__=="__main__":
-    if len(sys.argv)==1:
-        print '''
+
+if __name__ == "__main__":
+    if len(sys.argv) == 1:
+        print('''
     Available commands:
 
     2cca root       generate root identity
@@ -233,7 +246,7 @@ if __name__=="__main__":
 
     2cca crl        show crl
     2cca revoke
-'''
+''')
         raise SystemExit
 
     {'root':    generate_root,
@@ -243,4 +256,3 @@ if __name__=="__main__":
      'www':     generate_identity,
      'crl':     crl_show,
      'revoke':  revoke}[sys.argv[1]](get_config(sys.argv[1:]))
-
